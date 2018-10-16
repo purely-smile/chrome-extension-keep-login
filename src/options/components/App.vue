dbfcea43c77a5c36fffcfb92b258a9f7eabc038e
王宁 <wangning12@100tal.com>
2018-10-16 13:31:11 +0800

    调整配置中心css

--- src/options/components/App.vue
+++ src/options/components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="App-wrap">
     <div class="config-wrap">
-      <h3>配置中心</h3>
+      <h3 class="config-title">配置中心</h3>
       <alert :msg="msg"/>
       <input
         v-model="nick"
@@ -11,6 +11,12 @@
         v-model="password"
         type="text"
         placeholder="密码">
+      <input
+        type="number"
+        min="1"
+        max="20"
+        v-model="time"
+        placeholder="更新频率（分）">
       <button @click="save">保存</button>
     </div>
   </div>
@@ -27,27 +33,34 @@ export default {
       nick: '',
       password: '',
       msg: '',
+      time: '2',
     }
   },
   components: { alert },
   mounted() {
     const that = this
-    chrome.storage.sync.get('nick', (val) => {
-      that.nick = val.nick
-    })
-
-    chrome.storage.sync.get('password', (val) => {
-      that.password = val.password
+    chrome.storage.sync.get('config', (val) => {
+      if (typeof val.config !== 'object') return
+      Object.assign(that, val.config)
     })
   },
   methods: {
     save() {
-      const { nick, password } = this
+      const { nick, password, time } = this
       if (![nick, password].every(Boolean)) {
         this.msg = '请输入用户名和密码'
         return
       }
-      [{ nick }, { password }].forEach(val => saveData(val))
+      if (!time) {
+        this.msg = '请设置更新频率'
+        return
+      }
+      const config = {
+        nick,
+        password,
+        time,
+      }
+      saveData({ config })
       this.msg = '保存成功'
     },
   },
@@ -62,9 +75,34 @@ export default {
   border-radius: 3px;
   height: 100vh;
 }
+.config-title{
+  text-align: center;
+  margin-bottom: 10px;
+}
 .message-box{
   color:coral;
   margin-top: 1px;
   margin-bottom: 10px;
+}
+input {
+    width: 290px;
+    display: block;
+    border-radius: 3px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    padding: 5px;
+}
+button{
+    margin-top: 20px;
+    width: 100px;
+    text-align: center;
+    padding: 5px;
+    color: #fff;
+    background: #44acb6;
+    border: none;
+    border-radius: 3px;
+  &:hover{
+    background: #54c8cb;
+  }
 }
 </style>
